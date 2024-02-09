fe3d6d0a236117ec96965eeb597ff3dd25f45b90
Bidur <bibhabsahoobidur@gmail.com>
2024-02-09 19:14:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PracticeComponentComponent } from './practice-component/practice-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , PracticeComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/practice-component/practice-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practice-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './practice-component.component.html',
+  styleUrl: './practice-component.component.css'
+})
+export class PracticeComponentComponent {
+
+}
